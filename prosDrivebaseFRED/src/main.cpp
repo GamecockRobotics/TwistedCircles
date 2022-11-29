05eebc8c95b7b8fcfd118ac7cf5297413183cb19
Austin7856 <Wsanza@email.sc.edu>
2022-11-29 16:21:00 -0500

    added vision sensor for roller

--- prosDrivebaseFRED/src/main.cpp
+++ prosDrivebaseFRED/src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "pros/adi.hpp"
 #include "pros/llemu.hpp"
+#include "pros/optical.hpp"
 #include "pros/rotation.hpp"
 #include "pros/rtos.hpp"
 #include <iostream>
@@ -20,11 +21,13 @@
 #define CATAPULT_MAX 600
 #define LAUNCHER_PORT 'g'
 #define GYRO_PORT 13
+#define VISION_PORT 20
 enum intakeDirection { intake, outtake, stopped };
 intakeDirection intakeState = stopped;
 
 enum turnType{left, right};
 enum direction{forward, backward};
+enum color{red, blue};
 
 
 
@@ -41,6 +44,7 @@ enum direction{forward, backward};
 	pros::Motor Intake_2(INTAKE_PORT_2, true);
 	pros::Imu gyro (GYRO_PORT);
 	pros::ADIDigitalOut launcher(LAUNCHER_PORT);
+	pros::Optical vision(VISION_PORT);
 	
 	//Bumpers and switches example
 	pros::ADIDigitalIn SlipGearSensor (SLIPGEAR_BUMPER);
@@ -213,12 +217,35 @@ void turn(turnType dir, int32_t deg) {
   left_mtr3.brake();
 }
 
-void runRoller(){
-	Intake_1.move(70);
-	Intake_2.move(70);
-	pros::delay(1000);
-	Intake_1.move(0);
-	Intake_2.move(0);
+void runRoller(color color){	// 0 for blue and 1 for red
+	vision.set_led_pwm(100);
+	pros::lcd::set_text(4, std::to_string(vision.get_hue()));
+	if (color == red) {
+		while (!(vision.get_hue() > 300 || vision.get_hue() < 20)) {
+			Intake_1.move(70);
+			Intake_2.move(70);
+			pros::lcd::set_text(5, std::to_string(vision.get_hue()));
+		}
+		while (vision.get_hue() > 300 || vision.get_hue() < 20) {
+			Intake_1.move(70);
+			Intake_2.move(70);
+			pros::lcd::set_text(5, std::to_string(vision.get_hue()));
+		}
+	}
+	if (color == blue) {
+		while (!(vision.get_hue() < 300 && vision.get_hue() > 100)) {
+			Intake_1.move(70);
+			Intake_2.move(70);
+			pros::lcd::set_text(5, std::to_string(vision.get_hue()));
+		}
+		while (vision.get_hue() < 300 && vision.get_hue() > 100) {
+			Intake_1.move(70);
+			Intake_2.move(70);
+			pros::lcd::set_text(5, std::to_string(vision.get_hue()));
+		}
+	}
+	Intake_1.brake();
+	Intake_2.brake();
 }
 
 void runIntake(direction dir, float dis){
@@ -261,6 +288,14 @@ void shoot(){
 }
 
 void autonomous() {
+
+	runRoller(red);
+	pros::lcd::set_text(3, "Blue");
+	pros::delay(3000);
+	pros::lcd::set_text(3, "Red");
+	runRoller(blue);
+
+	/* 	
 	drive(-120, -50);
 	runRoller();
 	pros::delay(200);
@@ -271,6 +306,7 @@ void autonomous() {
 	pros::delay(200);
 	turn(left,270);
 	pros::delay(1000);
+	 */
 	//pros::delay(20000);
 	/*
 	drive(127,9000);
