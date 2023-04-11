b373fadeac8f4db026029b7c8ca0f37c648ad3f6
nchithan <chithannaveen@gmail.com>
2023-04-10 20:45:02 -0400

    Testing Odmentry

--- cursedFRED/src/main.cpp
+++ cursedFRED/src/main.cpp
@@ -10,24 +10,29 @@
 #include <iostream>
 #include <string>
 
-#define INTAKEL_PORT 6
+#define INTAKEL_PORT 15
 #define INTAKER_PORT 5
-#define CATAL_PORT 19
-#define CATAR_PORT 20
+#define CATAL_PORT 16
+#define CATAR_PORT 6
 #define GYRO_PORT 18
 #define LIMIT_PORT 'g'
 #define VISION1_PORT 11
 #define VISION2_PORT 12
-#define RANGE_SWITCH_PORT 'h' // These are pistons
-#define ENDGAME_PORT 'f'      // These are pistons
-#define CHASSIS_L1_PORT 7     // top front motor
-#define CHASSIS_L2_PORT 8     // Top back motor
-#define CHASSIS_L3_PORT 9     // bottom front motor
-#define CHASSIS_L4_PORT 10    // bottom back motor
-#define CHASSIS_R1_PORT 1     // top front motor
-#define CHASSIS_R2_PORT 2     // top back motor
-#define CHASSIS_R3_PORT 3     // bottom front motor
-#define CHASSIS_R4_PORT 4     // bottom back motor
+#define RANGE_SWITCH_PORT 'f' // These are pistons
+#define ENDGAME_PORT 'h'      // These are pistons
+#define CHASSIS_L1_PORT 18     // top front motor
+#define CHASSIS_L2_PORT 17     // Top back motor
+#define CHASSIS_L3_PORT 20     // bottom front motor
+#define CHASSIS_L4_PORT 19    // bottom back motor
+#define CHASSIS_R1_PORT 8     // top front motor
+#define CHASSIS_R2_PORT 7     // top back motor
+#define CHASSIS_R3_PORT 10     // bottom front motor
+#define CHASSIS_R4_PORT 9     // bottom back motor
+#define ROLLER_PORT 11
+
+// Define Ports for Sensors
+#define TRACKING_SIDE_PORT 14
+#define TRACKING_FORWARD_PORT 15
 
 enum color { red, blue };
 enum turnType { right, left };
@@ -43,24 +48,53 @@ pros::Motor chassis_l1(CHASSIS_L1_PORT);
 pros::Motor chassis_l2(CHASSIS_L2_PORT);
 pros::Motor chassis_l3(CHASSIS_L3_PORT, true);
 pros::Motor chassis_l4(CHASSIS_L4_PORT, true);
+pros::Motor roller(ROLLER_PORT);
 
-pros::Motor cataL(CATAL_PORT);
-pros::Motor cataR(CATAR_PORT, true);
-pros::Motor intake_1(INTAKEL_PORT, true);
-pros::Motor intake_2(INTAKER_PORT);
+pros::Motor cataL(CATAL_PORT, true);
+pros::Motor cataR(CATAR_PORT);
+pros::Motor intake_1(INTAKEL_PORT);
+pros::Motor intake_2(INTAKER_PORT, true);
 pros::ADIDigitalOut endgame(ENDGAME_PORT);
 pros::Imu gyro(GYRO_PORT);
 pros::ADIDigitalOut rangeSwitch(RANGE_SWITCH_PORT);
 pros::ADIDigitalIn SlipGearSensor(LIMIT_PORT);
 
+pros::Rotation tracking_side(TRACKING_SIDE_PORT);
+pros::Rotation tracking_forward(TRACKING_FORWARD_PORT);
+
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
-double ycord0 = 0;
-double xcord0 = 0;
-double ycord1 = 0;
-double xcord1 = 0;
-double theta = 0;
+// The value of pi
+static constexpr double pi = 3.1415926535897932;
+// The value of pi divided by 2
+static constexpr double pi2 = 1.5707963267948966;
+// Converts inches to millimeters
+static constexpr double inch_to_mm = 25.4;
+// Converts millimeters to inches
+static constexpr double mm_to_inch = 0.0393700787;
+// Converts degrees to radians
+static constexpr double degree_to_radian = 0.01745329252;
+// Converts centidegrees to radians
+static constexpr double centidegree_to_radian = .0001745329252;
+// Converts radians to degrees
+static constexpr double radian_to_degree = 57.2957795;
+// Measurement in mm of 1 centidegree of the tracking wheels
+static constexpr double track_wheel_size = 0.00609556241;
+// Distance from back tracking wheel to center of robot
+static constexpr double back_track_offset = 150;
+// The angle the robot is facing
+static constexpr double start_theta = 270;
+// The x coordinate of our alliance goal in millimeters
+static constexpr int goal_x = 457;
+// The y coordinate of our alliance goal in millimeters
+static constexpr int goal_y = 457;
 
+// Varaiables to keep track of the Location of the Robot
+double x_loc = 1385, y_loc = 3238;
+// Variable to keep track of the Orientation of the Robot
+double theta;
+
+int left_target = 0, right_target =0;
 bool endgameState = true;
 
 void tareMotors() {
@@ -75,11 +109,121 @@ void tareMotors() {
   chassis_r4.tare_position();
 }
 
-int odmentry() {
-  int value = 0;
-
-  return value;
+/**
+ * Gets the distance between two points
+ * 
+ * @param x0 the x coordinate of the first point
+ * @param y0 the y coordinate of the first point
+ * @param x1 the x coordinate of the second point
+ * @param y1 the y coordinate of the second point
+ */
+double get_distance(double x0, double y0, double x1, double y1) {
+	return sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1));
 }
+
+/**
+ * Gets the distance from the robot to the goal
+ *
+ * @return the distance in mm between the robot and the goal
+ */
+double get_goal_distance() {
+	return get_distance(x_loc, y_loc, goal_x, goal_y);
+}
+
+int odmentry() {
+  // Sets gyro rotation to starting angle of robot
+	gyro.set_rotation(start_theta);
+	// Gets the position of the tracking wheels
+	int track_side = tracking_side.get_position(), track_forward = tracking_forward.get_position();
+	// Stores the previous iteration of tracking wheel positions
+	int prev_track_side = track_side, prev_track_forward = track_forward;
+	// Sensor values for the movement of the tracking wheels.
+	double mm_forward = 0, mm_side = 0;
+	// Odometry control loop
+	while (true) {
+		// Get the sensor values for the tracking wheels and inertial sensor
+		track_forward = tracking_forward.get_position();
+		track_side = tracking_side.get_position();
+		// track_theta = gyro.get_rotation();
+
+		// Converts sensor values to respective standardized units (radians and mm)
+		mm_forward = (prev_track_forward - track_forward)*track_wheel_size;
+		mm_side = (track_side - prev_track_side)*track_wheel_size;
+
+		// Converts the local movements to the global position
+		y_loc -= cos(theta*degree_to_radian)*mm_side + sin(theta*degree_to_radian)*mm_forward;
+		x_loc += cos(theta*degree_to_radian)*mm_forward - sin(theta*degree_to_radian)*mm_side;
+		theta = gyro.get_rotation();
+
+		// Stores current sensor values to previous variables for use in next iteration
+		prev_track_forward = track_forward;
+		prev_track_side = track_side;
+
+		// Delay so that other tasks can run
+		pros::delay(10);
+
+		pros::lcd::set_text(0, 
+		"x: " + std::to_string((int)(x_loc*mm_to_inch)) + 
+		"    y: " + std::to_string((int)(y_loc*mm_to_inch)) + 
+		"    theta: " + std::to_string((int)theta));
+
+		
+	}
+	return 0;
+}
+
+void chassisL(int speed){
+  chassis_l1 = speed;
+  chassis_l2 = speed;
+  chassis_l3 = speed;
+  chassis_l4 = speed;
+}
+
+void chassisR(int speed){
+  chassis_r1 = speed;
+  chassis_r2 = speed;
+  chassis_r3 = speed;
+  chassis_r4 = speed;
+}
+
+/**
+ * Turns to face specific orientation
+ * Function is Blocking
+ *
+ * @param angle the angle that the robot should turn to
+ */
+void turn_to(double angle) {
+	double error = 180 - fmod((angle+180-(fmod(theta, 360))),360);
+	double prev_error;
+	double total_error = 0;
+	// The precision in degrees required to exit the loop
+	const double threshold = 1;
+	// The constants tuned for PID
+	// const double kp = 0.84, ki = 0.04, kd = 0.080;
+	const double kp = 1, ki = 0.05, kd = 2;
+
+	// PID control loop
+	while (fabs(error) > threshold || fabs(prev_error) > threshold) {
+		prev_error = error;
+		error = 180 - fmod((angle + 180 - (fmod(theta, 360))), 360);
+		total_error += (fabs(error) < 2 ? error : 0);
+
+		pros::lcd::set_text(2, "error: " + std::to_string(error));
+
+		left_target = -kp * error - kd * (error - prev_error) - ki * total_error;
+		right_target = kp * error + kd * (error - prev_error) + ki * total_error;
+    chassisL(left_target);
+    chassisR(right_target);
+		pros::delay(10);
+	}
+	// Zero out motors so the robot does not continue moving
+	left_target = 0;
+	right_target = 0;
+  chassisL(left_target);
+  chassisR(right_target);
+
+}
+
 
 void rangeSwitchToggle(bool rangeToggle) { rangeSwitch.set_value(rangeToggle); }
 
@@ -98,6 +242,7 @@ color get_color(double hue) {
   // Returns red or blue based off of the hue value seen by the vision sensor
   return (hue < 20) ? red : blue;
 }
+
 
 std::string intToString(int number) { return std::to_string(number); }
 
@@ -127,14 +272,12 @@ void initialize() {
 
   pros::c::serctl(SERCTL_DISABLE_COBS, NULL);
 
-  ycord0 = 0;
-  xcord0 = 0;
-  ycord1 = 0;
-  xcord1 = 0;
-  theta = 0;
+  
 
   cataL.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
   cataR.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+  rangeSwitch.set_value(false);
 }
 
 /**
@@ -166,7 +309,22 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+  while(true){
+    chassisR(127);
+    chassisL(-127);
+    pros::delay(5000);
+    chassisR(0);
+    chassisL(0);
+    pros::delay(1000);
+    chassisR(-127);
+    chassisL(127);
+    pros::delay(5000);
+    chassisR(0);
+    chassisL(0);
+    pros::delay(1000);
+  }
+}
 
 /**
 
@@ -230,6 +388,7 @@ void opcontrol() {
   int cataFlag = 0;
   pros::Task drive(driveTask);
   while (true) {
+    //controller.rumble("----...----");
 
     // Catapult pull back on Button Sensor
     if (!SlipGearSensor.get_value()) {
@@ -264,8 +423,11 @@ void opcontrol() {
       cataR.move_velocity(600);
     }
     if (controller.get_digital(DIGITAL_R2)) {
-      cataL.move_velocity(-600);
-      cataR.move_velocity(-600);
+      //cataL.move_velocity(-600);
+      //cataR.move_velocity(-600);
+      roller = 127;
+    } else {
+      roller = 0;
     }
 
     // // String Launcher
@@ -280,9 +442,9 @@ void opcontrol() {
     //   endgame.set_value(endgameState);
     // }
 
-    // if (controller.get_digital(DIGITAL_X)) {
-    //   rangeSwitchToggle(true);
-    // }
+    if (controller.get_digital(DIGITAL_X)) {
+      rangeSwitchToggle(true);
+    }
 
     pros::delay(20);
   }
